92d6152b7f0087fe6242a7d7b8d765ea20d5d7b1
AgusDav <davytagustin@gmail.com>
2023-10-09 13:19:21 -0300

    queda un error: el servlet de agregarUsuario no puede acceder al constructor de DtSocio

--- src/servlets/AgregarUsuario.java
+++ src/servlets/AgregarUsuario.java
@@ -1,26 +1,24 @@
 package servlets;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import datatypes.DtProfesor;
+import datatypes.DtSocio;
+import excepciones.UsuarioRepetidoException;
+import interfaces.Fabrica;
+import interfaces.IControlador;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import datatypes.DtProfesor;
-import datatypes.DtSocio;
-import datatypes.DtUsuario;
-import excepciones.UsuarioRepetidoException;
-import interfaces.Fabrica;
-import interfaces.IControlador;
+import java.io.IOException;
+import java.io.Serial;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-@WebServlet("/AgregarUsuario")
 public class AgregarUsuario extends HttpServlet {
+	@Serial
 	private static final long serialVersionUID = 1L;
        
     public AgregarUsuario() {
@@ -38,9 +36,9 @@ public class AgregarUsuario extends HttpServlet {
 		String nombre = request.getParameter("nombre");
 		String apellido = request.getParameter("apellido");
 		String fecha = request.getParameter("fecNac");
-		String tipo = request.getParameter("tipo");
+		String tipo = request.getParameter("tipoUsuario");
 		SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd");
-		Date fecNacDate= null;
+		Date fecNacDate;
 		try {
 			fecNacDate = sdf.parse(fecha);
 		} catch (ParseException e) {
@@ -50,14 +48,13 @@ public class AgregarUsuario extends HttpServlet {
 		IControlador icon = fabrica.getIControlador();
 		try {
 			if(tipo.equals("P")) {
-				String inst = null;
 				String bio = request.getParameter("biografia");
 				String desc = request.getParameter("descripcion");
 				String institucion = request.getParameter("institucion");
 				String web = request.getParameter("web");
 				DtProfesor dtP = new DtProfesor(nickname, nombre, apellido, email,password, fecNacDate, desc, bio, web);
-				icon.agregarProfesor(dtP, inst);
-			}else if(tipo.equals("S")){
+				icon.agregarProfesor(dtP, institucion);
+			}else if(tipo.equals("S")) {
 				DtSocio dts = new DtSocio(nickname, nombre, apellido, email, password, fecNacDate);
 				icon.agregarSocio(dts);
 			}
